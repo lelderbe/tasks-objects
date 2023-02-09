e3978d528a5ba6843e2d9b4ee44a4c779a99a054
evikary <daria_more10@mail.ru>
2023-02-09 12:15:48 +0300

    feat: add task01 solution

--- task01.js
+++ task01.js
@@ -4,4 +4,25 @@ const job = 'IT';
 
 // Собери все эти данные в одном объекте
 
+// const myObj = {};
+// myObj.name = name;
+// console.log(myObj);
+// myObj.age = age;
+// console.log(myObj);
+// myObj.job = job;
+
+// const myObj = {
+//   name: 'Joe',
+//   age: 21,
+//   job: 'IT',
+// };
+
+const myObj = {
+  name: name,
+  age: age,
+  job: job,
+};
+
 // и выведи объект в консоль
+
+console.log(myObj);
