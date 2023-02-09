44cd066fceee471ec65f1ed2ee1b27abbeced549
evikary <daria_more10@mail.ru>
2023-02-09 12:33:15 +0300

    feat: add task02 solution

--- task02.js
+++ task02.js
@@ -17,11 +17,24 @@ const mary = {
 };
 
 // выведи в консоль название (title) хобби joe и mary
+console.log(joe.hobbie.title);
+console.log(mary.hobbie.title);
 
 // добавь mary свойство 'job' со значением 'IT'
+// const job = 'IT';
+// mary.job = job;
+// console.log(mary);
+
+mary.job = 'IT';
 
 // выведи mary в консоль
 
+console.log(mary);
+
 // удали у joe свойство 'hobbie'
 
+delete joe.hobbie;
+
 // dsdtlb joe в консоль
+
+console.log(joe);
