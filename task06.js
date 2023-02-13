2232386a10c9fb8508d44af45a405395a443901d
evikary <daria_more10@mail.ru>
2023-02-13 09:36:16 +0300

    feat: add task06 solution

--- task06.js
+++ task06.js
@@ -4,4 +4,10 @@ const name = 'person';
 // Заполни массив people строками, чтобы получилось: [ 'person1', 'person2', 'person3' ]
 // Используй цикл for..i и переменную name для создания строк
 
+for (let i = 1; i <= 3; i++) {
+  people.push(name + i);
+}
+
 // выведи массив people в консоль
+
+console.log(people);
