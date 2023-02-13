9a62608a43eb04b4b15864292006156cb0b43ed7
evikary <daria_more10@mail.ru>
2023-02-13 10:59:56 +0300

    feat: add task09 solution

--- task09.js
+++ task09.js
@@ -12,4 +12,11 @@ const obj2 = {
 // Создай новый объект obj3, куда добавь все ключи (с их значениями) из obj1 и obj2
 // используй метод Object.assign
 
+const obj3 = Object.assign({}, obj1, obj2);
+
 // выведи obj3 в консоль
+
+console.log(obj3);
+console.log('----------------');
+console.log(obj2);
+console.log(obj1);
