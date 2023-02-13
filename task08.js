4bb37fb6a3a9f06770ee36495b04d3bf16358c7f
evikary <daria_more10@mail.ru>
2023-02-13 10:37:35 +0300

    feat: add task08 solution

--- task08.js
+++ task08.js
@@ -11,3 +11,9 @@ const obj2 = {
 
 // Добавь все ключи (с их значениями) из obj2 в obj1
 // используй for..in для перебора всех ключей в obj2
+
+for (let key in obj2) {
+  obj1[key] = obj2[key];
+  // console.log(obj1);
+}
+console.log(obj1);
